04b57018f274c26212c8460a0f07c64fccbc897e
unknown <tomuthu.engineer@gmail.com>
2024-03-01 01:28:26 +0530

    register functionality modified

--- APIs/auth/auth_controller.py
+++ APIs/auth/auth_controller.py
@@ -1,58 +1,54 @@
+from flask import request,jsonify
 
-
-def is_email_validate(email):
+def register_contorller(data):
     try:
-        if '@' in email:
-            return True,'Email valid'
-        else:
-            return False,'Email should contain @ character'
-    except ValueError as e:
-        return False,str(e)
+        data = request.json
+        username = data.get("userName")
+        email = data.get("email")
+        password = data.get("passWord")
+        confirm_password = data.get("confirmPassword")
 
-def is_password_validate(password):
-    try:
-        if len(password)<8:
-            return  False, "Password must be at least 8 characters long"
+        
+        if not username:
+            return jsonify({"error":"userName is Missing"}),400
+        if not email:
+            return jsonify({"error":"email is Missing"})
+        if not password:
+            return jsonify({"error":"passWord is Missing"}),400
+        if not confirm_password:
+            return jsonify({"error":"confirm_password is Missing"}),400
+        if not is_email_valid(email):
+            return jsonify({"error":"email should contain @ character "}),400
+        if password != confirm_password:
+            return jsonify({"error":"password do not match"}),400
 
-        if not any(char.isupper() for char in password):
-            return False, "Password must contain at least one uppercase letter"
-        
-        if not any(char.islower() for char in password):
-            return False, "Password must contain at least one lowercase letter"
-        
-        if not any(char.isdigit() for char in password):
-            return False, "Password must contain at least one digit"
-            
-        if password.isalnum():
-            return False, "Password must contain at least one special character"
-        
-        return True,"Password valid"
+        return jsonify({
+            "userName":username,
+            "passWord":password,
+            "confirmPassword":confirm_password,
+            "email":email
+        }),201
+
+    except Exception as e:
+        return jsonify({"error":str(e)}),500
     
-    except ValueError as e:
-        return False,str(e)
+def is_email_valid(email):
+    try:
+        if '@' not in email:
+            return False
+        return True
+    except Exception as e:
+        return jsonify({'error':str(e)}),500
 
-def is_confirm_password(password,confirm_pasword):
-    try :
-        if password != confirm_pasword:
-            return False,"Password do not match"
-        else :
-            return True,"Pasword Matched"
-    except ValueError as e :
-        return False,str(e)
-        
+# def is_password_valid(password):
+    try:
+        if not any(char.isdigit() for char in password):
+            raise ValueError('Password should contain one uppercase')
+        if not any(char.isupper() for char in password):
+            raise ValueError('Password should contain one uppercase')
+        return True
+    except:
+        pass
 
 
-def is_login_validate(login_userName,login_password):  
-    try :
-        
-        if username==login_userName and password==login_password:
-            return True ,"Login Sucessfull"
-        else :
-            return False ,"Login Unsucessfull"
-    except ValueError as e:
-        return False ,str(e)
-          
-
-        
-
-        
+    
--- APIs/auth/auth_view.py
+++ APIs/auth/auth_view.py
@@ -1,46 +1,23 @@
 '''Module providing Authentication functionalities'''
 from flask import request,jsonify
 from APIs.auth import auth_bp_v1
-from APIs.auth.auth_controller import is_email_validate,is_password_validate,is_confirm_password,is_login_validate
+from APIs.auth.auth_controller import register_contorller
 
 
 @auth_bp_v1.route('/register',methods=['POST'])
 def register_view():
     '''function returns response '''
     try:
-
         data = request.json
-        if not data or 'userName' not in data:
-            return jsonify({'Invalid Request Data':'Username is missing'}),400
-        elif not is_email_validate(data['email']) or 'email' not in data:
-            return jsonify({'Invalid Request Data':'Email is missing'}),400
-        elif not is_password_validate(data['password']) or 'password' not in data:
-            return jsonify({'Invalid Request Data':'Password is missing'}),400
+        return register_contorller(data)
+    except Exception as e:
+        return jsonify({"error":"Somthing Went Wrong"}),500
         
-        username = data.get('userName')
-
-        email = data.get('email') #email validation
-        is_valid_email ,message=is_email_validate(email)
-        if not is_valid_email:
-            return jsonify({'error':message}),400
         
-        password = data.get('password') #password validation
-        is_valid_password,message=is_password_validate(password)
-        if not is_valid_password:
-            return jsonify({'error':message}),400
-        
-        confirm_password = data.get('confirmPassword') #password revalidation
-        is_password_matched ,message = is_confirm_password(password,confirm_password)
-        if not is_password_matched:
-            return jsonify({'error':message}),400
-
-        
-        return jsonify({'userName':username,'email':email,'password':password,'confirmPassword':confirm_password}),201
-    except ValueError as e :
-        return jsonify({'error':str(e)}),500
     
-@auth_bp_v1.route("/login",methods=['POST'])
-def login_view():
+    
+# @auth_bp_v1.route("/login",methods=['POST'])
+# def login_view():
     try :
         data=request.json
         if not data or 'userName' not in data:
