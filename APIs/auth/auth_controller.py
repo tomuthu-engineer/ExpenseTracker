a57f8aa54696dc2380f6be22ba5da99059805077
unknown <tomuthu.engineer@gmail.com>
2024-03-08 13:06:46 +0530

    register validation updated

--- APIs/auth/auth_controller.py
+++ APIs/auth/auth_controller.py
@@ -8,8 +8,15 @@ def register_controller(data):
             if register_validate(data):
                 
                 user_name = data.get('user_name')
+                password = data.get('password')
+                email = data.get('email')
+                confirm_password = data.get('confirm_password')
+
             return jsonify({
-                'user_name':user_name
+                'user_name':user_name,
+                'password':password,
+                'email':email,
+                'confirm_password':confirm_password
                 }),201
         except ValueError as error:
             return jsonify({'error':str(error)}),400
--- APIs/auth/auth_helper.py
+++ APIs/auth/auth_helper.py
@@ -1,3 +1,5 @@
+from flask import jsonify
+
 def register_validate(data):
     
         user_name = data.get("user_name")
@@ -13,8 +15,12 @@ def register_validate(data):
             raise ValueError('Password is required')
         if confirm_password == None:
             raise ValueError('Confirm password is required')   
+        if password != confirm_password:
+            raise ValueError("Password don't match")
         if not is_email_valid(email):
             raise ValueError('Email is not valid ')
+        if  is_password_valid(password):
+             return jsonify({'error':password})
         return True
     
 
@@ -23,3 +29,11 @@ def is_email_valid(email):
     if '@' in email:
         return True
     
+def is_password_valid(password):
+    
+    if not any(char.isdigit() for char in password):
+      raise ValueError('Password should contain one digit character')
+    if not any(char.isupper() for char in password):
+        raise ValueError('Password should contain one uppercase character ')
+    if not any(char.islower() for char in password):
+        raise ValueError('Password should contain one lowercase character')
